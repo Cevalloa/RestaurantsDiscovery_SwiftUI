b3ab9c362b51ae5f42542ea79055e17390c47acd
Alex <cevalloa@gmail.com>
2026-09-06 21:10:40 -0400

    Adding test for restaurant count

--- RestaurantDiscoveryTests/RestaurantDiscoveryTests.swift
+++ RestaurantDiscoveryTests/RestaurantDiscoveryTests.swift
@@ -19,4 +19,14 @@ struct RestaurantDiscoveryTests {
         
         #expect(!searchableRestaurants.isEmpty)
     }
+    
+    @Test
+    @MainActor
+    func filteredRestaurants_defaultReturnsCount() async throws {
+        let viewModel = RestaurantListViewModel()
+        await viewModel.load()
+        let searchableRestaurants = viewModel.searchableRestaurants
+        
+        #expect(searchableRestaurants.count == 6)
+    }
 }
